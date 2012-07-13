34c0e65b97b0929283e906ade77e31d8e0fe50df
Bennett Wilson <bennettwilson@gmail.com>
2012-07-12 21:00:07 -0400

    initial commit after the bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bible::Application.config.secret_token = '87fb30afe10df40a7f7ae4845a391d48d72b6e81afbf377cc67417d0fb05b964699fe905b6c4cd3294d50c79020562e923f43cc099a45d1940efaff951b497fd'
