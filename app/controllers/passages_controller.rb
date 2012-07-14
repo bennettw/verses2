e0d6021f0eafb4bf7d4542a96598e40d8d474eb1
Bennett Wilson <bennettwilson@gmail.com>
2012-07-14 01:13:05 -0400

    adding a print out of verses on the passage show view. also added some sample data

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -14,6 +14,7 @@ class PassagesController < ApplicationController
   # GET /passages/1.json
   def show
     @passage = Passage.find(params[:id])
+    @verses = @passage.verses
 
     respond_to do |format|
       format.html # show.html.erb
