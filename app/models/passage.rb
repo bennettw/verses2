b80bf0e7d1b9e7581ae2a918313b23121a2f8282
Bennett Wilson <bennettwilson@gmail.com>
2012-09-03 16:08:26 -0400

    begin refactoring the esv api into its own lib file

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'rest-client'
-require 'rexml/document'
 
 class Passage < ActiveRecord::Base
   has_many :verses
@@ -11,58 +10,19 @@ class Passage < ActiveRecord::Base
   attr_reader :text
   attr_reader :doc
 
-  def user_reference=(str)
-    @user_reference = str
-    parse_reference  
+  def fetch(api=Esvapi.new)
+    api.query @user_reference
+    parse api.massage_query_results
   end
 
-  def parse_reference
-   # bad: make the api call here
-   # then when it returns get the verses from the api result
-   
-    response = RestClient.get 'http://www.esvapi.org/v2/rest/passageQuery', 
-      { 
-        :params => { 
-          :key => 'TEST',
-          :passage => @user_reference,
-          'output-format' => 'crossway-xml-1.0',
-          'include-line-breaks' => false
-        }
-      }
-
-    doc = REXML::Document.new response
-    @doc = doc
-    puts response
-    puts '========================='
-    @reference = doc.elements["*/passage/reference"].text
-    chapter = doc.elements["*/passage/surrounding-chapters/current"].text
-    puts "api ref = #{@reference}"
-    doc.elements.each("*/passage/content/verse-unit") do |elt|
-        # remove crap
-        # TODO: handle <woc> element that contains text, words of Christ!
-        #       handle html escape chars, like &dblquote;
-        elt.elements.delete_all 'heading'
-        elt.elements.delete_all 'marker'
-        elt.elements.delete_all 'begin-paragraph'
-        elt.elements.delete_all 'footnote'
-        elt.elements.delete_all 'woc/footnote'
-        elt.elements.delete_all 'end-paragraph'
-        elt.elements.delete_all 'begin-block-indent'
-        elt.elements.delete_all 'begin-line'
-        elt.elements.delete_all 'end-line'
-        elt.elements.delete_all 'end-block-indent'
-
-        verse_num = elt.elements['verse-num'].text
-        elt.elements.delete_all 'verse-num' 
-        verse_text = elt.children.join " "
-        verse_text.gsub! "<woc>", ""
-        verse_text.gsub! "</woc>", ""
-        puts "#{chapter}:#{verse_num}: #{verse_text}"
-        v = Verse.new
-        v.book = chapter
-        v.number = verse_num
-        v.text = verse_text
-        verses << v # TODO: throws an error if this object isn't saved 
+  def parse(results)
+    @reference = results[:reference]
+    @text = String.new
+    results[:verses].each do |verse|
+      v = Verse.new
+      v.chapter = verse[:chapter]
+      v.number = verse[:number]
+      @text << verse[:text]
     end
   end
 end
---
+++ lib/esvapi.rb
@@ -0,0 +1,58 @@
+require 'rexml/document'
+require 'rest-client'
+
+class Esvapi
+
+  BASE_URL = 'http://www.esvapi.org/v2/rest/passageQuery' 
+  API_KEY = 'TEST'
+
+  def query(ref)
+    # should this be a factory that makes EsvapiQuery objects, and those obejcts can be responsible for
+    # containing the different fields i care about
+
+    @response = RestClient.get BASE_URL,
+      { 
+        :params => { 
+          :key => API_KEY,
+          :passage => ref, 
+          'output-format' => 'crossway-xml-1.0',
+          'include-line-breaks' => false
+        }
+      }
+
+  end
+
+  def massage_query_results(response=@response)
+    doc = REXML::Document.new response
+    #puts response
+    #puts '========================='
+    reference = doc.elements["*/passage/reference"].text
+    chapter = doc.elements["*/passage/surrounding-chapters/current"].text
+    puts "api ref = #{reference}"
+    results = { :reference  => reference, :verses => [] }
+    
+    doc.elements.each("*/passage/content/verse-unit") do |elt|
+        # TODO:      handle html escape chars, like &dblquote;
+        elt.elements.delete_all 'heading'
+        elt.elements.delete_all 'marker'
+        elt.elements.delete_all 'begin-paragraph'
+        elt.elements.delete_all 'footnote'
+        elt.elements.delete_all 'woc/footnote'
+        elt.elements.delete_all 'end-paragraph'
+        elt.elements.delete_all 'begin-block-indent'
+        elt.elements.delete_all 'begin-line'
+        elt.elements.delete_all 'end-line'
+        elt.elements.delete_all 'end-block-indent'
+
+        verse_num = elt.elements['verse-num'].text
+        elt.elements.delete_all 'verse-num' 
+        verse_text = elt.children.join " "
+        verse_text.gsub! "<woc>", ""
+        verse_text.gsub! "</woc>", ""
+        puts "#{chapter}:#{verse_num}: #{verse_text}"
+        results[:verses] << { :chapter => chapter, :number => verse_num, :text => verse_text }
+    end
+
+    results
+  end
+end
